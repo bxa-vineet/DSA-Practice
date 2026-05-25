2d581d958e91fbe1b6cb0e1e815f36119b857166
bxa-vineet <vineet.jaiswal@bounteous.com>
2026-05-25 12:35:31 +0530

    Time: 0 ms (100%), Space: 46.8 MB (22.75%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] arr) {
+
+        int n=arr.length;
+        int i=0;
+        for(int j=1;j<n;j++){
+            if(arr[j]!=arr[i]){
+                i++;
+                arr[i]=arr[j];
+            }
+        }
+        return i+1;
+
+
+        
+    }
+}
