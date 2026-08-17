d3d9d9be86dd5f049bc075d75ec309cc7e26524f
bxa-vineet <vineet.jaiswal@bounteous.com>
2026-08-17 11:33:24 +0530

    Time: 15 ms (59.55%), Space: 51.5 MB (37.49%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,8 +2,8 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int i = g.length-1;
-        int j = s.length-1;
+        int i=g.length-1;
+        int j=s.length-1;
         int count = 0;
         while(i>=0 && j>=0){
             if(s[j]>=g[i]){
@@ -11,11 +11,10 @@ class Solution {
                 i--;
                 j--;
             }else{
-                 i--;
+                i--;
             }
-            
-          
         }
         return count;
+
     }
 }
